7032ffec62d3304e22b6786681e8dffb6b48fecd
JAYDIPSINH27 <jaydipadhiyar27@gmail.com>
2021-04-15 17:59:35 +0530

    User can add secrets.

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
 const userSchema=new mongoose.Schema({
     email:String,
     password:String,
-    googleId:String
+    googleId:String,
+    secret:String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -89,12 +90,51 @@ app.get("/register",function(req,res){
 });
 
 app.get("/secrets",function(req,res){
+    //show to all
+    // if(req.isAuthenticated()){
+    //     res.render("secrets");
+
+    // }else{
+    //     res.redirect("/login");
+    // }
+
+    User.find({"secret":{$ne:null}},function(err,foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets",{usersWithSecrets: foundUsers});
+            }
+        }
+    })
+});
+
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
 
     }else{
         res.redirect("/login");
     }
+});
+
+app.post("/submit",function(req,res){
+    const submittedSecret=req.body.secret;
+    User.findById(req.user.id,function(err,foundUser){
+        if(err)
+        {
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                foundUser.secret=submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                })
+            }
+        }
+    });
+
 });
 
 app.get("/logout",function(req,res){
